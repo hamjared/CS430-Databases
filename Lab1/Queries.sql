70d58d363751c410dc0142fad143dee5db6ac1bc
Jared <hamajred5@gmail.com>
2020-02-09 10:56:36 -0700

    q2 done

--- Lab1/Queries.sql
+++ Lab1/Queries.sql
@@ -1,3 +1,7 @@
 select first_name, last_name, salary
 from employees natural join salaries
 where salary > 90000 and to_date = '9999-01-01';
+
+select first_name, last_name, dept_name
+from (employees natural join current_dept_emp) natural join departments
+where dept_no = 'd009' or dept_no = 'd008';
