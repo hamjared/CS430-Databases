0f3fda09e14de181928ad18f40f44c9f8f3705eb
Jared <hamajred5@gmail.com>
2020-02-09 12:38:57 -0700

    q4 done

--- Lab1/Queries.sql
+++ Lab1/Queries.sql
@@ -8,4 +8,10 @@ where dept_no = 'd009' or dept_no = 'd008';
 
 select first_name, last_name, title
 from employees natural join titles
-where title = 'Technique Leader';
+where title = 'Technique Leader' and gender = 'F';
+
+select first_name, last_name, salary
+from (employees natural join titles) inner join salaries
+on employees.emp_no=salaries.emp_no and titles.to_date = salaries.to_date
+where salaries.to_date = '9999-01-01' and (not title = 'Senior Engineer')
+order by salary;
