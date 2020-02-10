fec62bfba6a5c5614a46628ff1d46a5055bc930f
Jared <hamajred5@gmail.com>
2020-02-10 12:24:23 -0700

    updated 4 to use join using instead of inner join

--- Lab1/Queries.sql
+++ Lab1/Queries.sql
@@ -11,9 +11,8 @@ from employees natural join titles
 where title = 'Technique Leader' and gender = 'F';
 
 select first_name, last_name, salary
-from (employees natural join titles) inner join salaries
-on employees.emp_no=salaries.emp_no and titles.to_date = salaries.to_date
-where salaries.to_date = '9999-01-01' and (not title = 'Senior Engineer')
+from (employees natural join titles) join salaries using(emp_no, to_date)
+where to_date = '9999-01-01' and (not title = 'Senior Engineer')
 order by salary;
 
 select first_name, last_name, birth_date
@@ -23,3 +22,6 @@ order by birth_date desc;
 select first_name, last_name
 from employees natural join dept_manager
 where to_date = '9999-01-01';
+
+select first_name, last_name
+from
