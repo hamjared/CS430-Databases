86cc8395092335c3cc73d7a2b5d5bf6328b42777
hamjared <hamjared5@gmail.com>
2020-03-14 22:51:26 -0600

    parsing for publisherToPhoneNumber is Done

--- Lab 2/PythonScripts/Publisher.py
+++ Lab 2/PythonScripts/Publisher.py
@@ -8,14 +8,20 @@ def commaParseFile(filename):
     line_list = f.read().splitlines()
     print (line_list)
     insertPublishers = 'INSERT INTO Publisher VALUES\n'
+    insertPublishersToPhoneNumber = 'INSERT INTO PublisherToPhoneNumber VALUES\n'
     for line in line_list:
         split_line = line.split(",")
         pubID = int(split_line[0])
         name = split_line[1][1:]
+        pubPhoneNumber = split_line[2][1:].split(" ")[0]
         insertPublishers += "({}, \'{}\'),\n".format(pubID, name)
+        insertPublishersToPhoneNumber += "({}, \'{}\'),\n".format(pubID, pubPhoneNumber)
 
     insertPublishers = insertPublishers[:-2] + ";"
+    insertPublishersToPhoneNumber = insertPublishersToPhoneNumber[:-2] + ";"
     print(insertPublishers)
+    print("---------------------------------------")
+    print(insertPublishersToPhoneNumber)
 
 
 
